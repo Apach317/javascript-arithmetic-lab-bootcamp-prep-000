0cc0e287a1c2ff1df03cc1074ad97145b23c302c
Ron Lynch <lynch.ronald1@gmail.com>
2018-06-25 03:09:09 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+add(1, 80);
+subtract(60, 40);
+multiply(2, 3.4);
+divide(5.0, 2.5);
+
+var number = 10
