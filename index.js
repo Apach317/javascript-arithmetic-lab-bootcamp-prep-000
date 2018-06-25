763dad2a221c303547788b6cc600f5174ebeeebb
Ron Lynch <lynch.ronald1@gmail.com>
2018-06-25 03:14:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,33 +19,6 @@ subtract(60, 40);
 multiply(2, 3.4);
 divide(5.0, 2.5);
 
-var number = 10
- 
-function add5() {
-  number += 5
-}
- 
-function divideBy3() {
-  number /= 3
-}
- 
-divideBy3()
- 
-console.log(number)
- 
-add5()
- 
-console.log(number)
- 
-number = 10
- 
-add5()
- 
-console.log(number)
- 
-divideBy3()
- 
-console.log(number)
 
 function inc(n) {
   return number++
